54c5bcefb097dcccd3100793517ed2bdf518cc40
Jiju Thomas <15jijuthomas98@gmail.com>
2024-04-06 19:23:24 +0530

    chore: firebased configs added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:melomix/app.dart';
 
-void main() {
+Future<void> main() async {
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const Melomix());
 }
